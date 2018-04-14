5e523e50639ab5b0572a8092d07731f9325dd5a9
edukng623 <edujorge13@gmail.com>
2018-04-14 12:38:05 +0100

    Using IntellIJ now

--- iWannaSurfApi/app.js
+++ iWannaSurfApi/app.js
@@ -1,23 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let mongoose = require('mongoose');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let debug = require('debug')('iwannasurfapi:app');
+let model = require('./model/model');
+let app = express();
+let connectionString = "mongodb://127.0.0.1:27017/test";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+mongoose.connect(connectionString).then( () => debug('Nigga We Made It')).catch( err => debug('Mah, Help Me!!' + err));
 
+let wade = new model.Player({name: "Dwyane Wade 2", team: "Miami Heat", age: 35});
+wade.save( () => debug("Wade was saved"));
+
+// let db = mongoose.createConnection('localhost', 'iWannaSurf',27017, {user: "edukng623", pass: "Thecarter@3"});
+// db.then( res => console.log("Look mah i connected")).catch( err => console.log('Look mah i failed you'));
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -35,7 +43,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.end('error');
 });
 
 module.exports = app;
