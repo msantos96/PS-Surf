120d998f80d0af2f251b6a9952f3ba94896336ac
msantos96 <marcosantoniosantos7@hotmail.com>
2018-05-23 17:36:20 +0100

    final

--- iWannaSurfApp/app/src/main/java/io/surf/wm/iwannasurfapp/AppCompatPreferenceActivity.kt
+++ iWannaSurfApp/app/src/main/java/io/surf/wm/iwannasurfapp/AppCompatPreferenceActivity.kt
@@ -12,7 +12,7 @@ import android.view.View
 import android.view.ViewGroup
 
 /**
- * Spot [android.preference.PreferenceActivity] which implements and proxies the necessary calls
+ * A [android.preference.PreferenceActivity] which implements and proxies the necessary calls
  * to be used with AppCompat.
  */
 abstract class AppCompatPreferenceActivity : PreferenceActivity() {
